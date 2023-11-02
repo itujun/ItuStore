d6ce2764572d8b8139f82bdbfd9ec9e0f0189b7b
itujun <itujun7@gmail.com>
2023-11-03 06:41:18 +0700

    Update file DetailProduct.jsx mengatur tampilan responsive

--- src/pages/Product/DetailProduct.jsx
+++ src/pages/Product/DetailProduct.jsx
@@ -29,20 +29,20 @@ export const DetailProductPage = () => {
       <StickyNavbar />
       <div className={`flex justify-center w-100 min-h-screen items-center ${isDarkMode === 'dark' ? 'bg-blue-gray-900' : ''}`}>
         {Object.keys(product).length > 0 ? (
-          <div className={`flex shadow-2xl lg:max-w-xl rounded-xl ${isDarkMode === 'dark' ? 'bg-blue-gray-500' : ''}`}>
-            <div className="flex font-sans">
-              <div className="flex-none relative w-36  ">
-                <img src={product.image} alt={product.title} className="absolute inset-0 w-full h-full object-cover object-center rounded-s-lg " loading="lazy" />
+          <div className={`flex shadow-2xl h-fit my-28 max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-5xl rounded-xl ${isDarkMode === 'dark' ? 'bg-blue-gray-500' : ''}`}>
+            <div className="flex flex-col md:flex-row font-sans">
+              <div className="md:w-6/12 lg:w-5/12 xl:w-4/12 flex items-center">
+                <img src={product.image} alt={product.title} className=" inset-0 w-full object-cover object-center rounded-s-lg " loading="lazy" />
               </div>
-              <form className="flex-auto p-6">
-                <div className="flex flex-wrap">
+              <form className="md:w-6/12 lg:w-7/12 xl:w-8/12 p-6 flex flex-col justify-between">
+                <div className="flex flex-col flex-wrap">
                   <Typography as="h1" variant="h5" className={`flex-auto font-semibold ${isDarkMode === 'dark' ? 'text-white' : 'text-slate-900'}`}>
                     {product.title}
                   </Typography>
-                  <Typography color={isDarkMode === 'dark' ? 'amber' : 'green'} className="font-bold">
+                  <Typography color={isDarkMode === 'dark' ? 'amber' : 'green'} className="font-bold mt-2">
                     ${product.price}
                   </Typography>
-                  <Typography className={`w-full flex-none text-sm font-medium mt-2 ${isDarkMode === 'dark' ? 'text-blue-gray-50' : 'text-slate-800'}`}>
+                  <Typography className={`w-full flex-none text-sm font-medium mt-0 ${isDarkMode === 'dark' ? 'text-blue-gray-50' : 'text-slate-800'}`}>
                     Review{' '}
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 mb-1 mr-1 text-amber-400 inline">
                       <path
@@ -53,30 +53,35 @@ export const DetailProductPage = () => {
                     </svg>
                     {product.rating.rate}/5 ({product.rating.count})
                   </Typography>
+                  <div className="flex flex-col items-baseline mt-4 mb-8 ">
+                    <Typography variant="small" className={`space-x-2 font-semibold block ${isDarkMode === 'dark' ? 'text-white' : 'text-slate-800'}`}>
+                      Description:
+                    </Typography>
+                    <Typography variant="small" className={`space-x-2 flex ${isDarkMode === 'dark' ? 'text-white' : 'text-slate-800'}`}>
+                      {product.description}
+                    </Typography>
+                  </div>
                 </div>
-                <div className="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-                  <Typography variant="small" className={`space-x-2 flex ${isDarkMode === 'dark' ? 'text-white' : 'text-slate-800'}`}>
-                    {product.description}
-                  </Typography>
-                </div>
-                <div className="flex space-x-4 mb-6 text-sm font-medium">
-                  <div className="flex-auto flex space-x-4">
-                    <Button variant="gradient" size="sm" color="amber" className="font-semibold rounded-md text-black" type="submit">
-                      Buy now
-                    </Button>
-                    <Button size="sm" variant="gradient" color="light-blue" className={`font-semibold rounded-md`} type="button" onClick={() => addToCart({ id, qty: 1 })}>
-                      Add to Cart
+                <div className="flex flex-col justify-between border-t-2 pt-6 border-slate-200">
+                  <div className="flex space-x-4 mb-6 text-sm font-medium">
+                    <div className="flex-auto flex space-x-4">
+                      <Button variant="gradient" size="sm" color="amber" className="font-semibold rounded-md text-black" type="submit">
+                        Buy now
+                      </Button>
+                      <Button size="sm" variant="gradient" color="light-blue" className={`font-semibold rounded-md`} type="button" onClick={() => addToCart({ id, qty: 1 })}>
+                        Add to Cart
+                      </Button>
+                    </div>
+                    <Button size="sm" className={`flex-none flex items-center justify-center  rounded-md text-slate-300 shadow border border-blue-gray-200 bg-transparent `} type="button" aria-label="Like">
+                      <svg width="20" height="20" fill="currentColor" aria-hidden="true" className="text-red-600 h-5 w-5">
+                        <path fillRule="evenodd" clipRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+                      </svg>
                     </Button>
                   </div>
-                  <Button size="sm" className={`flex-none flex items-center justify-center  rounded-md text-slate-300 shadow border border-blue-gray-200 bg-transparent `} type="button" aria-label="Like">
-                    <svg width="20" height="20" fill="currentColor" aria-hidden="true" className="text-red-600 h-5 w-5">
-                      <path fillRule="evenodd" clipRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
-                    </svg>
-                  </Button>
+                  <Typography variant="small" className={`${isDarkMode === 'dark' ? 'text-white' : 'text-blue-gray-900'}`}>
+                    Free shipping on all continental US orders.
+                  </Typography>
                 </div>
-                <Typography variant="small" className={`${isDarkMode === 'dark' ? 'text-white' : 'text-blue-gray-900'}`}>
-                  Free shipping on all continental US orders.
-                </Typography>
               </form>
             </div>
           </div>
